0279c36ee677693b576813053303bf3b06f26963
Katie Greer <katieggreer@gmail.com>
2024-02-12 20:22:47 -0700

    untracked file fix

---
+++ server/src/test/java/dataAccess/UserDAOTest.java
@@ -0,0 +1,12 @@
+package dataAccess;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDAOTest {
+
+    @Test
+    void createUser() {
+    }
+}
