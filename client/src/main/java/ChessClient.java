bf78eb5f82da7a6cab3578bba61344654934b74a
Katie Greer <katieggreer@gmail.com>
2024-03-10 21:00:34 -0600

    added start of repl class to handle console loop

---
+++ client/src/main/java/ChessClient.java
@@ -0,0 +1,6 @@
+public class ChessClient {
+
+    //functions to take input and call stuff from the serverFacade
+    private final ServerFacade server;
+    private final String serverUrl;
+}
---
+++ client/src/main/java/Repl.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import static ui.EscapeSequences.*;
+public class Repl {
+
+    //actual console stuff
+    private final ChessClient client;
+    private State state = State.SIGNEDOUT;
+
+
+    public Repl(String serverUrl) {
+        client = new ChessClient();
+    }
+    //serverUrl, this
+
+    public void run(){
+        System.out.println("Welcome to 240 chess. Type Help to get started.");
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine(); //get line
+            try {
+                result = client.eval(line); //evalute
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+            } catch (Throwable e) {
+                System.out.print(e.getMessage());
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + "[" + state + "]" + ">>> ");
+    }
+}
