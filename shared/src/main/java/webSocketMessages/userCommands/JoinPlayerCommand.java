c96fdb682109004158b11fa4bfcbb2ad747fbce3
Katie Greer <katieggreer@gmail.com>
2024-03-18 22:31:10 -0600

    filled out joinPlayerCommand a little more

--- shared/src/main/java/webSocketMessages/userCommands/JoinPlayerCommand.java
+++ shared/src/main/java/webSocketMessages/userCommands/JoinPlayerCommand.java
@@ -1,9 +1,17 @@
 package webSocketMessages.userCommands;
-
+import chess.ChessGame;
 public class JoinPlayerCommand extends UserGameCommand{
     //mmm session, playerusername, gameid, color?
-    public JoinPlayerCommand(String authToken) {
+    private int gameID;
+    private ChessGame.TeamColor color;
+
+    public JoinPlayerCommand(String authToken, Integer gameID, ChessGame.TeamColor playerColor) {
         super(authToken);
+        this.gameID = gameID;
+        this.color = playerColor;
 
     }
+    private int getGameID(){return gameID;}
+    private ChessGame.TeamColor getColor(){return color;}
 }
+//ummmmm
